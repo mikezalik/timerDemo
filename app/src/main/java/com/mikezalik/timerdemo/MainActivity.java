00fa73e6bd82494de6dadf26afbc851c8e238137
mikezalik <mike.zalik@gmail.com>
2020-01-30 14:30:18 -0800

    Adds handler and runnable.

--- app/src/main/java/com/mikezalik/timerdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/timerdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.mikezalik.timerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Hi", "A second passed by");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
     }
 }
